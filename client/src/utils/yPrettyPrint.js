bfa482986e640bd55bfebedb251c4ccc9851eeb4
rafcole <39354464+rafcole@users.noreply.github.com>
2023-07-03 19:05:19 -0700

    yPrettyPrint utility added for readable json of yDocs

---
+++ client/src/utils/yPrettyPrint.js
@@ -0,0 +1,7 @@
+import * as Y from "yjs";
+
+export const yPrettyPrint = (ydoc, msg = "") => {
+  console.log(
+    "\n\n==> " + msg + ": \n" + JSON.stringify(ydoc.toJSON(), null, 4) + "\n\n"
+  );
+};
