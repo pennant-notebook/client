8b58df62c8871aed5cf68a699918773c26b42065
rafcole <39354464+rafcole@users.noreply.github.com>
2023-07-13 09:35:13 -0700

    SPA routing and public folder lock down

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,15 @@ var cors = require("cors");
 
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(cors());
+
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("/*", express.static(path.join(__dirname, "../dist/index.html")));
+app.get(["/", "/*"], function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`Snack OverFlow Server listening on port: ${PORT}`)
