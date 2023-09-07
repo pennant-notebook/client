cb3ad5b5d7b2544f39d57182c4917040abedf89e
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-09-07 17:26:44 -0400

    clean up all remaining jsx and js files

--- src/components/UI/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = props => {
-  return (
-    <div className={`${props.asOverlay && 'loading-spinner__overlay'}`}>
-      <div className='lds-dual-ring'></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
