23afcba343b7553539709c71412fbfb06be55fed
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-10-04 16:49:09 -0500

    add Lato font to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: ['Lato']
+      }
+    }),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
